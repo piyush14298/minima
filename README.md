2e3afe0d2ab069ef2cc7b35ed6f6b1359fa53337
Saurya Velagapudi <saurya@gmail.com>
2021-03-04 18:30:03 +0800

    Update README to include commands on how to use, add CSS

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # minima
-A bunch of tools for futzing with the web
+A bunch of tools for futzing with the web so I can shoot videos of new features
+
+## TexTok
+### Hide Text
+Activate by selecting text and double-tapping control. A class will be added to make the text visible only on hover, slowly.
+
+Include in a page by copying the following text into the JavaScript console:
+   var script = document.createElement('script');
+   script.src = '//saurya.github.io/minima/textok/hide_text.css';
+   script.type = 'text/javascript';
+   document.getElementsByTagName('head')[0].appendChild(script);
+
---
+++ textok/hide_text.js
@@ -0,0 +1,42 @@
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-latest.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+var selectionScript = document.createElement('script');
+selectionScript.src = '//madapaja.github.io/jquery.selection/src/jquery.selection.js'
+selectionScript.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(selectionScript);
+
+var stylesheet = document.createElement('link');
+stylesheet.rel = "stylesheet"
+stylesheet.href= '//saurya.github.io/minima/textok/css/hide_text.css';
+document.getElementsByTagName('head')[0].appendChild(stylesheet);
+
+$(document).keydown(function(event) {
+    console.log(event);
+    if (!event.ctrlKey){ return true; }
+    if (document.ctrlPressed) {
+      // Get the active element + text selection
+      animate();
+      document.ctrlPressed = false;
+    } else {
+      document.ctrlPressed = true;
+    }
+    event.preventDefault();
+});
+
+
+animate = function(htmlSelection) {
+  $.selection('insert', {text: '<span class="animate_crazy">', mode: 'before'});
+  $.selection('insert', {text: '</span>', mode: 'after'});
+};
+
+// Insert CSS for animate_crazy
+
+// (Optional) Turn on Javascript context menu
+
+// TODO: Split the text into spans and re-insert into the document and add transitions to slowly make the spans visible again (ideally from left to right)
+
+// TODO: Use Web Audio to play Broken Coda
+
