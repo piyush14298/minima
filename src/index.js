b9ddad1f10fed3ef86e2f09027b22350b903767d
Mike Han <mmhan2u@gmail.com>
2021-04-16 12:34:26 +0630

    Simple implementation of separation of plugins

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import initHideText from './plugins/text';
+import initShiftClick from './plugins/images';
+
+console.log('script loaded');
+
+let ctrlPressed = false;
+let ctrlPressedListener = (event) => {
+  if(!event.ctrlKey){ return true; }
+
+  if(ctrlPressed){
+    // control pressed second time
+    ctrlPressed = false;
+    const event = new Event('minima.ctrlDoublePressed');
+    document.dispatchEvent(event);
+  }else{
+    ctrlPressed = true;
+    // ctrl needed to be double pressed within one second
+    setTimeout(() => ctrlPressed = false, 1000);
+  }
+};
+document.addEventListener('keydown', ctrlPressedListener);
+
+initHideText();
+initShiftClick();
---
+++ src/plugins/images/index.js
@@ -0,0 +1,18 @@
+// Simple replacement of images.
+// let all images be clickable
+let initShiftClick = () => {
+  let shiftClickListener = (event) => {
+    if(!event.shiftKey) { return true; }
+
+    event.target.src = "https://picsum.photos/200";
+    // some img tags come with srcset attribute
+    if(event.target.srcset){
+      event.target.srcset = "https://picsum.photos/200 1x"
+    }
+  }
+  Array.from(document.querySelectorAll('img')).forEach(function(img){
+    img.addEventListener('click', shiftClickListener);
+  });
+}
+
+module.exports = initShiftClick;
